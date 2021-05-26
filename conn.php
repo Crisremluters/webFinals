da744f8a62d14a3014bba8910ef577872b350b3e
Crisremluters <71479543+Crisremluters@users.noreply.github.com>
2021-05-26 21:07:35 +0800

    Update conn.php

--- conn.php
+++ conn.php
@@ -1,5 +1,5 @@
 <?php
-	$conn = mysqli_connect('sql6.freesqldatabase.com', 'sql6414736', '15hZmZpla1', 'sql6414736') or die(mysqli_error());
+	$conn = mysqli_connect('sql6.freesqldatabase.com', 'sql6414736', '15hZmZpIa1', 'sql6414736') or die(mysqli_error());
 	
 	if(!$conn){
 		die("Error: Failed to connect to database");
